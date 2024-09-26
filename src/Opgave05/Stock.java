6fac23577414375f93c7339ace08f804a1408263
blacksitegame <victor_rasmussen@live.dk>
2024-09-26 08:36:32 +0200

    SKole Done

--- src/Opgave05/Stock.java
+++ src/Opgave05/Stock.java
@@ -1,19 +1,26 @@
 package Opgave05;
 
 public class Stock {
+//    Atributter
     public String symbol;
     public String name;
     public double previousClosingPrice;
     public double currentPrice;
-
+// Constructor
     public Stock (String symbol, String name, double previousClosingPrice, double currentPrice){
         this.symbol = symbol;
         this.name = name;
         this.previousClosingPrice = previousClosingPrice;
         this.currentPrice = currentPrice;
     }
-
+// getChangePercent metode
     public double getChangePercent (){
         return ((currentPrice - previousClosingPrice) / previousClosingPrice) * 100;
+    }
+// print metode
+    public void print (){
+        System.out.println(symbol + " " + name);
+        System.out.println(previousClosingPrice + " " + currentPrice);
+        System.out.println(getChangePercent());
     }
 }
--- src/Opgave05/StockTest.java
+++ src/Opgave05/StockTest.java
@@ -2,12 +2,10 @@ package Opgave05;
 
 public class StockTest {
     public static void main(String[] args) {
+//        Opretter stock
         Stock stock = new Stock("ORCL", "Oracle",34.5, 34.35);
 
-        System.out.println("Symbol: " + stock.symbol);
-        System.out.println("Name: " + stock.name);
-        System.out.println("Previous Closing price: " + stock.previousClosingPrice);
-        System.out.println("Current price: " + stock.currentPrice);
-        System.out.println("percentage gain or loss: " + stock.getChangePercent());
+//        Printer variabler til konsollen
+        stock.print();
     }
 }
